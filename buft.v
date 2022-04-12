63605090d251b5a9ba88c0c0656fc4db29b212ba
C-Vaishnavi <60243502+C-Vaishnavi@users.noreply.github.com>
2022-04-12 09:37:19 +0530

    Create buft.v

---
+++ buft.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    03:19:40 04/12/2022 
+// Design Name: 
+// Module Name:    buf 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module buft( data1 , data2 , inp , clk
+    );
+input[7:0] inp;
+input clk;
+output reg[7:0] data1 , data2 ;
+
+always @(posedge clk)
+   data2 = inp;
+	
+always @(negedge clk)
+	data1 = data2;
+	
+
+endmodule
